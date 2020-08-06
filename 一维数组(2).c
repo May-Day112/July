92f1f22790913cd5fb819bd06ea5ac49b0fe4d19
May-Day112 <68180185+May-Day112@users.noreply.github.com>
2020-08-06 14:26:47 +0800

    Create 一维数组(2).c
    利用数组计算斐波那契数列的前20个数，并按每行打印5个数的格式输出。
    斐波那契数列，又称黄金分割数列，指的是这样一个数列：1、1、2、3、5、8、13、21、34……在数学上，斐波纳契数列以如下被以递归的方法定义：F（1）=1，F（1）=1，F（n）=F(n-1)+F(n-2)（n≥3，n∈N*）

---
+++ 一维数组(2).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{int i;
+int f[20]={1,1};//初始化f[0],f[1]
+for(i=2;i<20;i++)
+{f[i]=f[i-1]+f[i-2];
+}
+for(i=0;i<20;i++)
+{printf("%6d",f[i]);
+if((i+1)%5==0)
+{printf("\n"); //每输出5个数字就换行
+}
+return 0;
+}
